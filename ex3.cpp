0b3d3f7055930b43a7f0ef1dd35f444f1727532a
Muhammad umar <153982980+Umarkeerio@users.noreply.github.com>
2024-11-07 18:32:03 +0500

    Add files via upload

---
+++ ex3.cpp
@@ -0,0 +1,16 @@
+// Write a C++ program to Find Sum of Diagonal Elements of Matrix
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr1[3][3] = {{3,0,0}, {0,2,0}, {0,0,1}};
+    int total = 0;
+
+    int j = 0; 
+    for (int i = 0; i < 3; i ++){
+            total += arr1[i][j];
+            j++;
+    }
+    cout << total;
+}
